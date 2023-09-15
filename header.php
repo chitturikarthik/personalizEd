c536e6426a9c911534a091c6d9f003fce7f16903
Karthik Chitturi <karthik_csd@srkrec.edu.in>
2023-09-16 00:40:54 +0530

    kcpc

---
+++ header.php
@@ -0,0 +1,83 @@
+<?php 
+echo '
+
+<div class="header-body border-top-0 bg-dark box-shadow-none">
+    <div class="header-container container-fluid">
+        <div class="header-row p-relative px-0">
+            <div class="header-column px-lg-3">
+                <div class="header-row">
+                    <div class="header-logo">
+                        <a href="index.html"><img alt="Porto" width="120" height="58" data-sticky-width="82" data-sticky-height="40" data-sticky-top="0" src="img/logo-default-slim-dark.png"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="header-column w-100 ms-2 ms-xl-5 ps-2 pe-lg-3">
+                <div class="header-row justify-content-end justify-content-lg-start">
+                    <div class="header-nav header-nav-links header-nav-light-text justify-content-lg-start">
+                        <div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-mobile-dark header-nav-main-dropdown-border-radius header-nav-main-text-capitalize header-nav-main-text-size-5 header-nav-main-arrows header-nav-main-effect-1 header-nav-main-sub-effect-1">
+                            <nav class="collapse">
+                                <ul class="nav nav-pills" id="mainNav">
+                                    <li class="dropdown">
+                                        <a class="dropdown-item active" href="index.html">
+                                            Home
+                                        </a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a class="dropdown-item" data-hash data-hash-offset="0" data-hash-offset-lg="90" href="index.html#demos">
+                                            About Us
+                                        </a>
+                                    </li>
+                                    <li class="dropdown dropdown-mega">
+                                        <a class="dropdown-item" href="elements.html">
+                                            Topics
+                                        </a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a class="dropdown-item" href="#">
+                                            Features
+                                        </a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a class="dropdown-item" href="#">
+                                            Pages
+                                        </a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a class="dropdown-item" href="#">
+                                            Portfolio
+                                        </a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a class="dropdown-item" href="#">
+                                            Blog
+                                        </a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a class="dropdown-item" href="#">
+                                            Shop
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="d-none d-xxl-flex custom-header-1-extra-links">
+                            <ul class="nav me-3 mt-1 ">
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-outline btn-primary mb-2">Register</a>
+                                </li>
+                            </ul>
+
+                            <a href="#" class="btn btn-primary mb-2">Login</a>
+
+                        </div>
+                        <button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav"><i class="fa fa-bars"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+';
+?>

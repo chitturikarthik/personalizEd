56dcb139496d14b0377c59e37ddab55b11da4aea
Karthik Chitturi <karthik_csd@srkrec.edu.in>
2023-09-16 01:25:15 -0700

    header changes

--- header.php
+++ header.php
@@ -24,7 +24,7 @@ echo '
 									<nav class="header-nav-top">
 									<ul class="nav nav-pills text-uppercase text-2">
 										<li class="nav-item nav-item-anim-icon d-none d-md-block">
-											<a class="nav-link ps-0" href="about-us.html"><i class="fas fa-angle-right"></i> About Us</a>
+											<a href="#" class="btn btn-outline btn-rounded btn-primary py-2 px-3 m-2">Sign Up	</a>
 										</li>
 										<li class="nav-item nav-item-anim-icon d-none d-md-block">
 											<a class="nav-link" href="contact-us.html"><i class="fas fa-angle-right"></i> Contact Us</a>

cb3bca45d7eccddf82829c4fc4dc216f86618434
Tejesh Budde <btejesh_csd@srkrec.edu.in git config --global user.name>
2023-09-17 14:24:48 +0530

    BT1

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
 									<div class="owl-carousel owl-theme nav-style-3 nav-light dots-light text-color-light" data-plugin-options="{'items': 1, 'margin': 100, 'loop': true, 'nav': true, 'dots': true, 'stagePadding': 60, 'autoHeight': true}">
 										<div class="text-center pt-3">
 											<strong class="font-weight-extra-bold text-13 text-lg-14">100<sup class="sup-text-small ps-1">%</sup></strong>
-											<label class="font-weight-normal text-3 px-lg-4">Percent of happy users using Porto</label>
+											<label class="font-weight-normal text-3 px-lg-4"> of happy users using Porto</label>
 										</div>
 										<div class="text-center pt-3">
 											<strong class="font-weight-extra-bold text-13 text-lg-14">50<sup class="sup-text-small ps-1">+</sup></strong>

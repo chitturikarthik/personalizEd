fa86e307e988e1db1d9d015f2734c14b8353fbd3
Sathwik Pulihora <sathwikjrs_csd@srkrec.edu.in>
2023-09-19 14:26:43 +0530

    sathwikquote

--- index.php
+++ index.php
@@ -301,7 +301,7 @@
 					<div class="col-lg-4 px-5 pt-2 appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="800">
 						<div class="testimonial testimonial-style-7 testimonial-primary py-5 mt-4">
 							<blockquote>
-								<p class="text-6 line-height-9 mb-0 mt-4">Your time is limited, so don’t waste it living someone else’s life.</p>
+								<p class="text-6 line-height-9 mb-0 mt-4">Personalize your learning based on your learning intelligence.</p>
 							</blockquote>
 							<a class="btn btn-outline btn-light text-color-light text-color-hover-dark font-weight-bold mb-3" href="#">LEARN MORE</a>
 						</div>

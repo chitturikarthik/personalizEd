5cf9228d1759f31659a05514e2d4800f42514f93
Pallavi Patient <pallavireddi925@gmail.com>
2023-09-19 15:22:05 +0530

    sahithi

--- index.php
+++ index.php
@@ -133,7 +133,7 @@
 							</div>
 							<div class="flip-back d-flex align-items-center p-5" style="background-image: url(img/generic/generic-corporate-17-1.jpg); background-size: cover; background-position: center;">
 								<div class="flip-content my-4">
-									<h4 class="font-weight-bold text-color-light">Second Step</h4>
+									<h4 class="font-weight-bold text-color-light">Content</h4>
 									<p class="font-weight-light text-color-light opacity-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa enim. Nullam id varius.</p>
 									<a href="#" class="btn btn-light btn-modern text-color-dark font-weight-bold">View content</a>
 								</div>

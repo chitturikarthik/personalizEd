8fbef72df9efa17dff2d3260168f9266d1d0418c
Sathwik Pulihora <sathwikjrs_csd@srkrec.edu.in>
2023-09-19 21:06:05 +0530

    added img

--- index.php
+++ index.php
@@ -213,12 +213,12 @@
 					<div class="col-lg-4 px-lg-1 mb-3 appear-animation" data-appear-animation="fadeInDownShorter" data-appear-animation-delay="600">
 						<span class="thumb-info thumb-info-no-borders thumb-info-no-borders thumb-info-lighten thumb-info-bottom-info thumb-info-bottom-info-dark thumb-info-bottom-info-show-more thumb-info-no-zoom">
 							<span class="thumb-info-wrapper overlay overlay-show overlay-op-8">
-								<img src="img/gallery/gallery-9.jpg" class="img-fluid" alt="">
+								<img src="img/gallery/c-logo.png" class="img-fluid" alt="">
 								<span class="thumb-info-title bg-transparent mb-4 ps-4 ms-2">
-									<span class="thumb-info-type font-weight-light text-1 line-height-1 opacity-8">LOREM IPSUM DOLOR SIT</span>
+									<span class="thumb-info-type font-weight-light text-1 line-height-1 opacity-8"></span>
 									<span class="thumb-info-inner font-weight-bold text-4 line-height-4">C Programming</span>
 									<span class="thumb-info-show-more-content pe-3">
-										<p class="mb-0 text-1 line-height-9 pe-5 pb-3 me-5 mb-3 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa enim. Nullam id varius nunc. Vivamus bibendum magna ex.</p>
+										<p class="mb-0 text-1 line-height-9 pe-5 pb-3 me-5 mb-3 mt-2"></p>
 										<a href="#" class="d-inline-flex align-items-center btn btn-light text-color-dark font-weight-bold px-2 btn-py-1 text-1 rounded"><span class="text-2 py-1 ps-2">LEARN MORE</span> <i class="fa fa-arrow-right ms-2 me-2 ps-4 text-3"></i></a>
 									</span>
 								</span>

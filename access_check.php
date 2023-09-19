4e235b9d212a06bbd235b98a5e6f6bbb92d47be5
Karthik Chitturi <karthik_csd@srkrec.edu.in>
2023-09-19 23:29:28 +0530

    teacherrevert

--- access_check.php
+++ access_check.php
@@ -1,22 +1,32 @@
-<?php
+<?php 
 session_start();
 
 if (isset($_POST['accesscheck'])) {
   $email = $_POST['username'];
   $mobile = $_POST['pwd'];
+  $role = $_POST['role']; // Get the selected role
+
   include 'connect.php';
 
-  $stmt = $conn->prepare("SELECT * FROM student_details WHERE email = ? AND mobile = ?");
-  $stmt->bind_param("ss", $email, $mobile);
+  $stmt = $conn->prepare("SELECT * FROM student_details WHERE email = ? AND mobile = ? AND role = ?");
+  $stmt->bind_param("ssi", $email, $mobile, $role); // Update the binding
   $stmt->execute();
   $result = $stmt->get_result()->fetch_assoc();
 
   if ($result) {
-    // Valid credentials, set the session and redirect to student_dashboard.php
+    // Valid credentials, set the session and redirect based on the role
     $_SESSION["access"] = "personalized";
     $_SESSION["user-id"] = $email;
-    header("Location: student_dashboard.php");
-    exit();
+    
+    if ($role == 0) {
+      // Student role, redirect to student_dashboard.php
+      header("Location: student_dashboard.php");
+      exit();
+    } elseif ($role == 1) {
+      // Teacher role, redirect to teacher_dashboard.php (replace with actual teacher page)
+      header("Location: ./Teacher/index.php");
+      exit();
+    }
   } else {
     // Invalid credentials, redirect to index.php
     header("Location: index.php");
@@ -32,3 +42,4 @@ if (isset($_POST['accesscheck'])) {
   header("Location: index.php");
   exit();
 }
+?>
